99933201a1443423e60e34054aaf31ca5a5c8768
Money Yu <abc12207@gmail.com>
2025-02-19 17:27:59 +0800

    add app insight support

--- src/ScribbleOpeAIAnalysis/Program.cs
+++ src/ScribbleOpeAIAnalysis/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient(); // Register HttpClient
+
+builder.Services.AddOpenTelemetry().UseAzureMonitor(o =>
+{
+    o.ConnectionString =
+        "InstrumentationKey=37b302d2-3cc5-4036-8327-ca4d02a7b89a;IngestionEndpoint=https://australiaeast-1.in.applicationinsights.azure.com/;LiveEndpoint=https://australiaeast.livediagnostics.monitor.azure.com/;ApplicationId=f4ca2a35-cb18-44ef-a97f-637a2abef975";
+});
 
 var app = builder.Build();
 
