3203ccd10c920b58e338ae879a81aabe398b08a9
Dipanjan Ghosh (AUS) <dighosh@microsoft.com>
2025-04-08 16:32:32 +1000

    addded spinner to the pages

--- src/ScribbleOpeAIAnalysis/wwwroot/js/site.js
+++ src/ScribbleOpeAIAnalysis/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#loading-spinner").show();
+});
+
+$(window).on("load", function () {
+    $("#loading-spinner").hide();
+});
